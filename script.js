eec6b1136062e7f4f57e3de131247f16290dc4a9
antmar0417 <tonyboy@live.se>
2021-12-08 17:09:51 +0100

    fetching data from splash

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let count = 5;
+const count = 5;
 const apiKey = 'XuMYouBC7zDdVoDKQuZz38cIER7sZoivTbq_b1wt7cQ';
 let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+getPhotos();
