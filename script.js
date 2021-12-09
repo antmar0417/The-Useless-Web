c2a316e2a35a18e7c3374bcae97ebbcdadf1d213
antmar0417 <tonyboy@live.se>
2021-12-09 13:04:53 +0100

    checking the frame width

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let count = 0;
+let intFrameWith = window.innerWidth;
+
+// console.log(intFrameWith);
+if (intFrameWith >= 1024) {
+  count = 9;
+}
+if (intFrameWith < 1024 && intFrameWith >= 768) {
+  count = 6;
+}
+if (intFrameWith < 768) {
+  count = 3;
+}
+console.log(count);
+
+// let widthOutput = document.querySelector('#site-title');
+// window.addEventListener('resize', () => {
+//   widthOutput = window.innerWidth;
+//   // console.log(widthOutput);
+//   if (widthOutput >= 1024) {
+//     count = 6;
+//   }
+//   if (widthOutput < 1024 && widthOutput >= 768) {
+//     count = 4;
+//   }
+//   if (widthOutput < 768) {
+//     count = 3;
+//   }
+//   console.log(count);
+// });
+
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
@@ -8,7 +39,6 @@ let photosArray = [];
 let initialLoad = true;
 
 // Unsplash API
-let count = 6;
 const apiKey = 'XuMYouBC7zDdVoDKQuZz38cIER7sZoivTbq_b1wt7cQ';
 let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -47,7 +77,7 @@ function dispplayPhotos() {
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
-      title: photo.alt_description,
+      title: `likes: ${photo.likes}`,
     });
     img.addEventListener('load', imageLoaded);
     item.appendChild(img);
@@ -60,7 +90,7 @@ async function getPhotos() {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     dispplayPhotos();
-    //console.log(photosArray);
+    console.log(photosArray);
   } catch (error) {
     //Catch Error Here
   }
